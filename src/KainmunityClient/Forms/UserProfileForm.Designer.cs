31c65cae290e58cd79d0e73fdcfa8b96b5ffaf0e
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2024-05-05 00:02:11 +0800

    Create UserProfileForm

---
+++ src/KainmunityClient/Forms/UserProfileForm.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace KainmunityClient.Forms
+{
+    partial class UserProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contactNumber = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.homeAddress = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.yearlyIncome = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.householdSize = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(254)))), ((int)(((byte)(247)))));
+            this.panel1.Controls.Add(this.back);
+            this.panel1.Controls.Add(this.save);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.householdSize);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.yearlyIncome);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.homeAddress);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.contactNumber);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.emailAddress);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.firstName);
+            this.panel1.Controls.Add(this.lastName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(152, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(515, 449);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(67)))));
+            this.label1.Location = new System.Drawing.Point(175, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PROFILE";
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(278, 143);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(115, 20);
+            this.lastName.TabIndex = 1;
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(123, 143);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(149, 20);
+            this.firstName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(275, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // emailAddress
+            // 
+            this.emailAddress.Location = new System.Drawing.Point(123, 183);
+            this.emailAddress.Name = "emailAddress";
+            this.emailAddress.Size = new System.Drawing.Size(270, 20);
+            this.emailAddress.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(120, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(120, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Contact Number";
+            // 
+            // contactNumber
+            // 
+            this.contactNumber.Location = new System.Drawing.Point(123, 232);
+            this.contactNumber.Name = "contactNumber";
+            this.contactNumber.Size = new System.Drawing.Size(270, 20);
+            this.contactNumber.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(120, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Home Address";
+            // 
+            // homeAddress
+            // 
+            this.homeAddress.Location = new System.Drawing.Point(123, 279);
+            this.homeAddress.Name = "homeAddress";
+            this.homeAddress.Size = new System.Drawing.Size(270, 20);
+            this.homeAddress.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(120, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Yearly Income";
+            // 
+            // yearlyIncome
+            // 
+            this.yearlyIncome.Location = new System.Drawing.Point(123, 328);
+            this.yearlyIncome.Name = "yearlyIncome";
+            this.yearlyIncome.Size = new System.Drawing.Size(149, 20);
+            this.yearlyIncome.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(275, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Household Size";
+            // 
+            // householdSize
+            // 
+            this.householdSize.Location = new System.Drawing.Point(278, 328);
+            this.householdSize.Name = "householdSize";
+            this.householdSize.Size = new System.Drawing.Size(115, 20);
+            this.householdSize.TabIndex = 13;
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(93)))), ((int)(((byte)(102)))));
+            this.save.FlatAppearance.BorderSize = 0;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.ForeColor = System.Drawing.Color.White;
+            this.save.Location = new System.Drawing.Point(194, 370);
+            this.save.Margin = new System.Windows.Forms.Padding(2);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(120, 40);
+            this.save.TabIndex = 15;
+            this.save.Text = "SAVE";
+            this.save.UseVisualStyleBackColor = false;
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(254)))), ((int)(((byte)(247)))));
+            this.back.FlatAppearance.BorderSize = 0;
+            this.back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.back.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(67)))));
+            this.back.Location = new System.Drawing.Point(14, 14);
+            this.back.Margin = new System.Windows.Forms.Padding(2);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(52, 23);
+            this.back.TabIndex = 16;
+            this.back.Text = "BACK";
+            this.back.UseVisualStyleBackColor = false;
+            // 
+            // UserProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(176)))), ((int)(((byte)(170)))));
+            this.ClientSize = new System.Drawing.Size(802, 502);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "UserProfileForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox householdSize;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox yearlyIncome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox homeAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox contactNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox emailAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button save;
+    }
+}
