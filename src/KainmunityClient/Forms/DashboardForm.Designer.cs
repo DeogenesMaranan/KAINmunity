414591f8ba7359c981878dfa42e3a58f2983d450
Admin <Admin@DESKTOP-A4PQ45I>
2024-05-07 19:54:52 +0800

    Add Dashboard

--- src/KainmunityClient/Forms/DashboardForm.Designer.cs
+++ src/KainmunityClient/Forms/DashboardForm.Designer.cs
@@ -28,87 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.firstName = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm));
             this.donationBox = new System.Windows.Forms.Button();
             this.requestButton = new System.Windows.Forms.Button();
             this.logoutButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // firstName
-            // 
-            this.firstName.AutoSize = true;
-            this.firstName.Location = new System.Drawing.Point(64, 12);
-            this.firstName.Name = "firstName";
-            this.firstName.Size = new System.Drawing.Size(51, 13);
-            this.firstName.TabIndex = 1;
-            this.firstName.Text = "<default>";
-            this.firstName.Click += new System.EventHandler(this.firstName_Click);
             // 
             // donationBox
             // 
-            this.donationBox.Location = new System.Drawing.Point(294, 12);
+            this.donationBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(93)))), ((int)(((byte)(102)))));
+            this.donationBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.donationBox.ForeColor = System.Drawing.Color.White;
+            this.donationBox.Location = new System.Drawing.Point(3, 3);
             this.donationBox.Name = "donationBox";
-            this.donationBox.Size = new System.Drawing.Size(75, 23);
+            this.donationBox.Size = new System.Drawing.Size(157, 43);
             this.donationBox.TabIndex = 2;
-            this.donationBox.Text = "Donate";
-            this.donationBox.UseVisualStyleBackColor = true;
+            this.donationBox.Text = "DONATE";
+            this.donationBox.UseVisualStyleBackColor = false;
             this.donationBox.Click += new System.EventHandler(this.donationBox_Click);
             // 
             // requestButton
             // 
-            this.requestButton.Location = new System.Drawing.Point(375, 12);
+            this.requestButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(93)))), ((int)(((byte)(102)))));
+            this.requestButton.Font = new System.Drawing.Font("Tw Cen MT Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.requestButton.ForeColor = System.Drawing.Color.White;
+            this.requestButton.Location = new System.Drawing.Point(166, 3);
             this.requestButton.Name = "requestButton";
-            this.requestButton.Size = new System.Drawing.Size(75, 23);
+            this.requestButton.Size = new System.Drawing.Size(157, 43);
             this.requestButton.TabIndex = 3;
-            this.requestButton.Text = "Request";
-            this.requestButton.UseVisualStyleBackColor = true;
+            this.requestButton.Text = "REQUEST";
+            this.requestButton.UseVisualStyleBackColor = false;
             this.requestButton.Click += new System.EventHandler(this.requestButton_Click);
             // 
             // logoutButton
             // 
-            this.logoutButton.Location = new System.Drawing.Point(455, 12);
+            this.logoutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(93)))), ((int)(((byte)(102)))));
+            this.logoutButton.Font = new System.Drawing.Font("Tw Cen MT Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutButton.ForeColor = System.Drawing.Color.White;
+            this.logoutButton.Location = new System.Drawing.Point(329, 3);
             this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(75, 23);
+            this.logoutButton.Size = new System.Drawing.Size(157, 43);
             this.logoutButton.TabIndex = 4;
-            this.logoutButton.Text = "Logout";
-            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Text = "LOGOUT";
+            this.logoutButton.UseVisualStyleBackColor = false;
             this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(93)))), ((int)(((byte)(102)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.firstName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(818, 52);
+            this.panel1.TabIndex = 6;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(176)))), ((int)(((byte)(170)))));
+            this.flowLayoutPanel1.Controls.Add(this.donationBox);
+            this.flowLayoutPanel1.Controls.Add(this.requestButton);
+            this.flowLayoutPanel1.Controls.Add(this.logoutButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 52);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(818, 50);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DASHBOARD";
+            // 
+            // firstName
+            // 
+            this.firstName.AutoSize = true;
+            this.firstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstName.ForeColor = System.Drawing.Color.White;
+            this.firstName.Location = new System.Drawing.Point(715, 13);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(94, 25);
+            this.firstName.TabIndex = 1;
+            this.firstName.Text = "<default>";
+            this.firstName.Click += new System.EventHandler(this.firstName_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(609, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(100, 25);
             this.label1.TabIndex = 5;
             this.label1.Text = "Welcome,";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Dashboard
+            // panel3
+            // 
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 102);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(818, 439);
+            this.panel3.TabIndex = 8;
+            // 
+            // DashboardForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 299);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.logoutButton);
-            this.Controls.Add(this.requestButton);
-            this.Controls.Add(this.donationBox);
-            this.Controls.Add(this.firstName);
-            this.Name = "Dashboard";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(254)))), ((int)(((byte)(247)))));
+            this.ClientSize = new System.Drawing.Size(818, 541);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DashboardForm";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load_1);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label firstName;
         private System.Windows.Forms.Button donationBox;
         private System.Windows.Forms.Button requestButton;
         private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label firstName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
