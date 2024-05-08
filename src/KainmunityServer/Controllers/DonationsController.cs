128cc442825548922ee3194c5d5f784fe80ce395
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2024-05-08 13:08:01 +0800

    Revert "Add feature (feedback submission) api"
    This reverts commit 3d8f6855045663a57b6715baaf3c4c89c68e5bdd.

--- src/KainmunityServer/Controllers/DonationsController.cs
+++ src/KainmunityServer/Controllers/DonationsController.cs
@@ -36,21 +36,5 @@ namespace KainmunityServer.Controllers
             var donations = await DonationsManager.GetAvailable();
             return new JsonResult(Ok(donations));
         }
-
-        [HttpPost("submit")]
-        public async Task<JsonResult> SubmitFeedback(FeedbackItem feedbackItem)
-        {
-            var isSuccess = await DonationsManager.AddFeedback(feedbackItem);
-            return new JsonResult(isSuccess ? Ok() : Unauthorized());
-        }
-
-        [HttpGet("feedback")]
-        public async Task<JsonResult> GetAllFeedbacks()
-        {
-            var donations = await DonationsManager.GetFeedback();
-            return new JsonResult(Ok(donations));
-        }
-
-
     }
 }
--- src/KainmunityServer/Models/FeedbackItem.cs
+++
@@ -1,9 +0,0 @@
-namespace KainmunityServer.Models
-{
-    public class FeedbackItem
-    {
-        public int RespondentId { get; set; }
-        public int Star { get; set; }
-        public string Feedback { get; set; }
-    }
-}
