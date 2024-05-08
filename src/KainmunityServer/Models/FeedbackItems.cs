287f64833407a1a5b7351122d39a8f5d9792e075
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2024-05-08 13:16:28 +0800

    Create FeedbackFeature: form, controller, manager, model

---
+++ src/KainmunityServer/Models/FeedbackItems.cs
@@ -0,0 +1,9 @@
+namespace KainmunityServer.Models
+{
+    public class FeedbackItems
+    {
+        public int? FeedbackId { get; set; }
+        public int RespondentId { get; set; }
+        public string Content {  get; set; }
+    }
+}
