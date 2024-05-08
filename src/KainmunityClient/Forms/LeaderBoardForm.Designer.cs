07eb5e7e2665c7e3165830209c673b58b1174b42
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2024-05-08 19:30:49 +0800

    Add Leaderboard automatic placeholders

--- src/KainmunityClient/Forms/LeaderBoardForm.Designer.cs
+++ src/KainmunityClient/Forms/LeaderBoardForm.Designer.cs
@@ -29,40 +29,85 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LeaderBoard));
-            this.label4 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel2.SuspendLayout();
+            this.title = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.titlePanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.leaderboardContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel.SuspendLayout();
+            this.titlePanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label4
+            // title
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(67)))));
-            this.label4.Location = new System.Drawing.Point(11, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(290, 60);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "LEADERBOARD";
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(67)))));
+            this.title.Location = new System.Drawing.Point(11, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(290, 60);
+            this.title.TabIndex = 13;
+            this.title.Text = "LEADERBOARD";
             // 
-            // panel1
+            // panel
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(176)))), ((int)(((byte)(170)))));
-            this.panel1.Location = new System.Drawing.Point(184, 48);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(443, 373);
-            this.panel1.TabIndex = 14;
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.AutoScroll = true;
+            this.panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(176)))), ((int)(((byte)(170)))));
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.leaderboardContainer);
+            this.panel.Location = new System.Drawing.Point(184, 48);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(443, 373);
+            this.panel.TabIndex = 14;
             // 
-            // panel2
+            // titlePanel
             // 
-            this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Location = new System.Drawing.Point(254, 24);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(309, 61);
-            this.panel2.TabIndex = 14;
+            this.titlePanel.BackColor = System.Drawing.Color.Transparent;
+            this.titlePanel.Controls.Add(this.title);
+            this.titlePanel.Location = new System.Drawing.Point(254, 24);
+            this.titlePanel.Name = "titlePanel";
+            this.titlePanel.Size = new System.Drawing.Size(309, 61);
+            this.titlePanel.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(140, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(331, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Donations";
+            // 
+            // leaderboardContainer
+            // 
+            this.leaderboardContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.leaderboardContainer.AutoScroll = true;
+            this.leaderboardContainer.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.leaderboardContainer.Location = new System.Drawing.Point(13, 81);
+            this.leaderboardContainer.Name = "leaderboardContainer";
+            this.leaderboardContainer.Size = new System.Drawing.Size(415, 289);
+            this.leaderboardContainer.TabIndex = 3;
             // 
             // LeaderBoard
             // 
@@ -72,22 +117,27 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.titlePanel);
+            this.Controls.Add(this.panel);
             this.DoubleBuffered = true;
             this.Name = "LeaderBoard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "LeaderBoardForm";
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.Text = "Leaderboard";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.titlePanel.ResumeLayout(false);
+            this.titlePanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Panel titlePanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel leaderboardContainer;
     }
 }
--- src/KainmunityClient/Forms/LeaderBoardForm.cs
+++ src/KainmunityClient/Forms/LeaderBoardForm.cs
@@ -16,5 +16,40 @@ namespace KainmunityClient.Forms
         {
             InitializeComponent();
         }
+
+        private void showLeaderboardEntry(int donorId, string donorName, int donations)
+        {
+            TextBox name = new TextBox();
+            name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(176)))), ((int)(((byte)(170)))));
+            name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            name.Font = new System.Drawing.Font("Tw Cen MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            name.Name = $"name_{donorId}";
+            name.Size = new System.Drawing.Size(300, 21);
+            name.Text = donorName;
+            name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+
+            TextBox donationCount = new TextBox();
+            donationCount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(176)))), ((int)(((byte)(170)))));
+            donationCount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            donationCount.Font = new System.Drawing.Font("Tw Cen MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            donationCount.Name = $"donations_{donorId}";
+            donationCount.Size = new System.Drawing.Size(96, 21);
+            donationCount.TabIndex = 1;
+            donationCount.Text = "1000";
+            donationCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+
+            TableLayoutPanel entryContainer = new TableLayoutPanel();
+            entryContainer.ColumnCount = 2;
+            entryContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            entryContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            entryContainer.Controls.Add(name, 0, 0);
+            entryContainer.Controls.Add(donationCount, 1, 0);
+            entryContainer.Name = $"entryContainer_{donorId}";
+            entryContainer.RowCount = 1;
+            entryContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            entryContainer.Size = new System.Drawing.Size(408, 32);
+
+            leaderboardContainer.Controls.Add(entryContainer);
+        }
     }
 }
